c72a83798d59c528df6a870dda8071d165ff6818
Kohsin <102904030+Kohsin@users.noreply.github.com>
2022-04-10 00:59:22 +0800

    Update cycle_reservoir_layer.py

--- codes_ViR/ViR/reservoir/cycle_reservoir_layer.py
+++ codes_ViR/ViR/reservoir/cycle_reservoir_layer.py
@@ -4,7 +4,6 @@ import numpy as np
 import scipy.sparse as sp
 import random
 
-cishu = 0
 def init_reservoir(units, cycle_weight, jump_weight, jump_size, connection_weight):
     sparse_matrix = torch.ones(units, units) * connection_weight
     random_sign = torch.rand(units, units)
@@ -55,7 +54,7 @@ class Cycle_Reservoir(torch.nn.Module):
         self.spectral_radius = spectral_radius
         self.leaky = leaky
         self.sparsity = sparsity
-
+        self.cishu = 0
         self.kernel = torch.Tensor(init_weight(input_size, self.units, sparsity * self.input_scaling))
         self.kernel = nn.Parameter(self.kernel, requires_grad=False)
 
@@ -80,11 +79,10 @@ class Cycle_Reservoir(torch.nn.Module):
         x = xt.clone().detach()
         input_part = torch.mm(x, self.kernel)
         state_part = torch.tanh(torch.mm(h_prev, self.recurrent_kernel) + input_part)
-        cishu=cishu+1
-        print(cishu)
+        self.cishu=self.cishu+1
+        print(self.cishu)
         output = torch.cat([x, h_prev * (1 - self.leaky) + state_part], dim=1)
         reservoir_output = torch.tanh(self.weight_out(output))
-        print("")
         reservoir_output1 = torch.cat([x, state_part, reservoir_output,
                                       x ** 2, state_part ** 2,  reservoir_output ** 2], dim=1)
         reservoir_output2 = torch.tanh(self.weight(reservoir_output1))
