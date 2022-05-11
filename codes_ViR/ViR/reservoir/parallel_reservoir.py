fcdd8af6f9a9d7a6ff04f1f80d137f7e12da7692
Kohsin <102904030+Kohsin@users.noreply.github.com>
2022-05-12 01:56:37 +0800

    Update parallel_reservoir.py

--- codes_ViR/ViR/reservoir/parallel_reservoir.py
+++ codes_ViR/ViR/reservoir/parallel_reservoir.py
@@ -14,7 +14,7 @@ class Residual(nn.Module):
 class PreNorm(nn.Module):
     def __init__(self,dim, fn):
         super().__init__()
-        self.norm = nn.LayerNorm(dim)
+        self.norm = nn.LayerNorm(dim*2)
         self.fn = fn
     def forward(self, x, **kwargs):
         return self.fn(self.norm(x), **kwargs)
